472da504f543d987ef64e86fca0028b51c762626
dmitrykudin <dim271096@gmail.com>
2017-12-20 16:59:28 +0300

    Registration started

--- Performances/Performances.Client/ViewModel/MainViewModel.cs
+++ Performances/Performances.Client/ViewModel/MainViewModel.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Performances.Client.Model;
+using Performances.Model;
 
 namespace Performances.Client.ViewModel
 {
     public class MainViewModel : BaseModel
     {
         private MainWindow _window;
+        public User MainUser { get; set; }
         public ScreenTypes CurrentScreenType { get; set; }
+
+        public MainViewModel()
+        {
+            MainUser = new User();
+        }
 
         public MainWindow Window
         {
@@ -24,6 +31,20 @@ namespace Performances.Client.ViewModel
             }
         }
 
+        private object _currentContent;
+        public object CurrentContent
+        {
+            get { return _currentContent; }
+            set
+            {
+                if (_currentContent != value)
+                {
+                    _currentContent = value;
+                    RaisePropertyChanged(nameof(CurrentContent));
+                }
+            }
+        }
+        
         public void SetScreen()
         {
             IScreen screen;
---
+++ Performances/Performances.Client/ViewModel/RegistrationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using Performances.Client.View;
+using Performances.Model;
+
+namespace Performances.Client.ViewModel
+{
+    public class RegistrationViewModel
+    {
+        public User RegisterUser { get; set; }
+        public CreativeTeam RegisterCreativeTeam { get; set; }
+        
+        public UserRegistrationView uRegistrationView { get; set; } 
+        public CreativeTeamRegistrationView ctRegistrationView { get; set; }
+        
+
+        public RegistrationViewModel()
+        {
+            
+        }
+
+
+    }
+}
