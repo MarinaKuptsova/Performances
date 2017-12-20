a204b0e313a488705287194c77f143de776c60d9
dmitrykudin <dim271096@gmail.com>
2017-12-20 22:23:44 +0300

    ImageByteConverter added

---
+++ Performances/Performances.Client/Helpers/ImageByteConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Performances.Client.Helpers
+{
+    public class ImageByteConverter
+    {
+        public BitmapImage ImageFromBuffer(byte[] bytes)
+        {
+            MemoryStream stream = new MemoryStream(bytes);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = stream;
+            image.EndInit();
+            return image;
+        }
+
+        public byte[] BufferFromImage(BitmapImage imageSource)
+        {
+            Stream stream = imageSource.StreamSource;
+            byte[] buffer = null;
+            if (stream != null && stream.Length > 0)
+            {
+                using (BinaryReader br = new BinaryReader(stream))
+                {
+                    buffer = br.ReadBytes((Int32)stream.Length);
+                }
+            }
+            return buffer;
+        }
+    }
+}
