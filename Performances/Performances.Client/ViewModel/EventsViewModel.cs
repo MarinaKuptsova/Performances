96ea211690149bfba73a3e429ba7f7cd206324ad
MarinaKuptsova <marina55596@yandex.ru>
2017-12-20 23:27:55 +0300

    Add event view and viewmodel

---
+++ Performances/Performances.Client/ViewModel/EventsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Performances.Client.Model;
+using Performances.Model;
+
+namespace Performances.Client.ViewModel
+{
+    public class EventsViewModel : BaseModel, IScreen
+    {
+        public object CurrentDialog { get; set; }
+        public MainViewModel Parent { get; set; }
+        public ObservableCollection<Event> AllEvents { get; set; }
+        public BitmapImage Photo { get; set; }
+
+        public EventsViewModel(MainViewModel mvm)
+        {
+            Parent = mvm;
+        }
+        public void Initialize()
+        {
+            //var result = DataAccess.DataAccess.GetAllEvents();
+            //AllEvents = new ObservableCollection<Event>(result);
+        }
+
+        public object View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
